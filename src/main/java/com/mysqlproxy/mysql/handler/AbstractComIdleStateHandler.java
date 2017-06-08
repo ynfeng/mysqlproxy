77cabce1166974e5b249540642e2d9c5ec47487f
fengyannan <fynwin@gmail.com>
2017-06-08 14:27:31 +0800

    没实现的命令提示

--- src/main/java/com/mysqlproxy/mysql/handler/AbstractComIdleStateHandler.java
+++ src/main/java/com/mysqlproxy/mysql/handler/AbstractComIdleStateHandler.java
@@ -2,8 +2,13 @@ package com.mysqlproxy.mysql.handler;
 
 
 import com.mysqlproxy.mysql.MysqlConnection;
+import com.mysqlproxy.mysql.codec.ErrorPacketEncoder;
 import com.mysqlproxy.mysql.protocol.CommandType;
+import com.mysqlproxy.mysql.protocol.ErrorPacket;
+import com.mysqlproxy.mysql.state.ComIdleState;
 import com.mysqlproxy.mysql.state.ComQueryState;
+
+import java.io.IOException;
 
 
 public abstract class AbstractComIdleStateHandler implements StateHandler {
@@ -15,7 +20,36 @@ public abstract class AbstractComIdleStateHandler implements StateHandler {
                 connection.setDirectTransferPacketLen(packageLength + 4);
                 connection.setState(ComQueryState.INSTANCE);
                 connection.drive(connection.getReadBuffer());
+            } else {
+                responseError(connection,"What's the fuck? This command seems not implements yet!");
             }
+        }
+    }
+
+    private void responseError(MysqlConnection connection,String msg){
+        try {
+            if(connection.isWriteMode()){
+                if(connection.flushWriteBuffer()){
+                    connection.getWriteBuffer().clear();
+                    connection.getReadBuffer().clear();
+                    connection.disableWriteAndEnableRead();
+                    connection.setState(ComIdleState.INSTANCE);
+                    return;
+                }
+            }
+            int packetLength = 9 + msg.length();
+            byte sequenceId = 1;
+            ErrorPacket errorPacket = new ErrorPacket(packetLength,sequenceId);
+            errorPacket.header = (byte) 0xFF;
+            errorPacket.errCode = 1064;
+            errorPacket.sqlStateMarker = "#";
+            errorPacket.sqlState = "42000";
+            errorPacket.errMsg = msg;
+            connection.disableRead();
+            connection.writePacket(errorPacket, ErrorPacketEncoder.INSTANCE);
+        } catch (IOException e) {
+            //TODO 异常处理
+            e.printStackTrace();
         }
     }
 }
